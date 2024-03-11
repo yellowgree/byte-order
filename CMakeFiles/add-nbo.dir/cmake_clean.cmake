4c9a265be484428013443c42bc5db9472da00eb5
haeun <gkds0103@naver.com>
2024-03-11 00:05:49 -0400

    Add CMakeFile

---
+++ CMakeFiles/add-nbo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/add-nbo.dir/add-nbo.c.o"
+  "CMakeFiles/add-nbo.dir/add-nbo.c.o.d"
+  "add-nbo"
+  "add-nbo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/add-nbo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/add-nbo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for add-nbo.
+# This may be replaced when dependencies are built.
